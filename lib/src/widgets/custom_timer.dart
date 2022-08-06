6aeac55d172ff0c23b288e40f76b661cc4056e5a
Stanley-Ipkiss <70805429+Stanley-Ipkiss@users.noreply.github.com>
2022-08-06 17:06:02 +0300

    Update custom_timer.dart

--- lib/src/widgets/custom_timer.dart
+++ lib/src/widgets/custom_timer.dart
@@ -122,7 +122,7 @@ class _CustomTimerState extends State<CustomTimer>
       }
     }
 
-    if (widget.onChangeState != null) widget.onChangeState!(state);
+    if (widget.onChangeState == finished) widget.onChangeState!(state);
   }
 
   void updateController(AnimationStatus status) {
